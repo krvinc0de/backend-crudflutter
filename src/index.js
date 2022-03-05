0fb249b875b2e4036c12f2ebe1b1a7de8da56f9a
Kevin <polopolopolopoline@gmail.com>
2022-03-05 00:43:30 -0600

    v2
    estaba usando un constructor cuando no era requerido y cambie el puerto para que se vea mas bonito en el navegador

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const { app } = require('./app');
+const app = require('./app');
 const { dbConection } = require('./database');
 
 async function main(){
         //conectarse a db
         await dbConection();
         //iniico sv
-        await app.listen(4002);
+        await app.listen(4000);
 }
 
 main();
