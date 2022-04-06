2914872627c663631779368e64698c1ea069d962
Kevin <polopolopolopoline@gmail.com>
2022-04-05 19:28:11 -0500

    Nuevo dato recibido
    Agregue el nuevo dato recibido desde el formulario "panel rm"

--- src/models/cliente.model.js
+++ src/models/cliente.model.js
@@ -3,6 +3,7 @@ const clienteSchema = new Schema({
         nombre_negocio: String,
         categoria: String,
         Direccion: String,
+        Eslogan: String,
         descripccion_negocio: String,
         descripccion_dos: String,
         Num_telefonico: String,          
